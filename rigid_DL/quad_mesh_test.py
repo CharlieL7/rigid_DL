b7f9969746e6e28768ae7edc3656ce398ee96de3
Charlie-Ik <charlielin7@gmail.com>
2020-08-18 13:09:10 -0400

    Moved files

--- rigid_DL/quad_mesh_test.py
+++ rigid_DL/quad_mesh_test.py
@@ -2,15 +2,12 @@
 Quadratic mesh test
 """
 
-import math
-import time
 import argparse as argp
 import numpy as np
 import meshio
 import simple_quad_mesh as sqm
-import gauss_quad as gq
-import geometric as geo
 import eigenfunctions as efun
+import input_output as io
 import potential_calc as poc
 
 def main():
@@ -25,7 +22,11 @@ def main():
 
     quad_mesh = sqm.simple_quad_mesh(verts, faces)
     C = poc.make_mat_cp_qe(quad_mesh)
-    print(C)
+    v_cnst = np.array([1, 0, 0])
+    v_trans_in = efun.make_translation_vels(v_cnst, quad_mesh, 'c')
+    v_trans_out = np.dot(C, v_trans_in.flatten('C'))
+    v_trans_out = v_trans_out.reshape(v_trans_in.shape, order='C')
+    io.write_vel(v_trans_in, v_trans_out, "quad_test_trans_vel.csv")
 
 if __name__ == "__main__":
     main()

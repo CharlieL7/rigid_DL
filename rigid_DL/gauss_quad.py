Finally fixed the normal calculation error! You took the derivative of phi_0 incorrectly... left the 1 term...

rigid_DL/gauss_quad.py
@@ -1,5 +1,5 @@
 """
-Seven point Gaussian quadrature for abritrary f(xi, eta, nodes) function
+Gaussian quadrature for abritrary f(xi, eta, nodes) function
 """
 
 import numpy as np
@@ -17,6 +17,7 @@ PARA_PTS = [
     (0.0, 0.5),
     (1./3., 1./3.),
 ]
+
 
 def int_over_tri_lin(func, nodes, hs):
     """
@@ -71,7 +72,7 @@ def int_over_tri_quad_n(func, nodes, n):
                expecting f(eta, xi, nodes)
         nodes: 3x6 ndarray with nodes as column vectors
         n: normal vectors at the seven quadrature points (3, 7) ndarray
-            (not normalized vectors)
+            (not normalized vectors, hs magnitude)
     Returns:
         integrated (function . n)
     """
@@ -82,7 +83,7 @@ def int_over_tri_quad_n(func, nodes, n):
     return ret
 
 
-def quad_n(nodes):
+def quad_n(nodes, xc, tri_c):
     """
     Calculate the normal vector values over a curved triangle for severn point Gaussian quadrature.
     This function is use to reduce the number of times these values are recalculated.
@@ -94,7 +95,77 @@ def quad_n(nodes):
     """
     normals = np.empty([3, 7])
     for i, (xi, eta) in enumerate(PARA_PTS):
+        e_xi = np.einsum("ij,j->i", nodes, geo.dphi_dxi_quadratic(xi, eta, nodes))
+        e_eta = np.einsum("ij,j->i", nodes, geo.dphi_deta_quadratic(xi, eta, nodes))
+        n = np.cross(e_xi, e_eta)
+        if np.dot(n, tri_c - xc) < 0.:
+            print("n reori")
+            n = -n
+        normals[:, i] = n
+    return normals
+
+
+def int_over_tri_quad_slow(func, nodes, xc, tri_c):
+    """
+    Integrate a function over a curved triangle surface using Gaussian quadrature.
+    Seven point quadrature.
+    SLOW VERSION.
+
+    Parameters:
+        func : function to integrate, can return any order tensor
+               expecting f(eta, xi, n, nodes)
+        nodes : 3x6 ndarray with nodes as column vectors
+        xc: mesh centroid
+        tri_c: triangle center
+    Returns:
+        integrated function
+    """
+    # Gaussian quadrature weights
+    w = 1./60. * np.array([3., 8., 3., 8., 3., 8., 27.])
+
+    # Gaussian quadrature points
+    para_pts = [
+        (0.0, 0.0),
+        (0.5, 0.0),
+        (1.0, 0.0),
+        (0.5, 0.5),
+        (0.0, 1.0),
+        (0.0, 0.5),
+        (1./3., 1./3.),
+    ]
+
+    """
+    # 6-point quadrature
+    a = 0.816847572980459
+    b = 0.445948490915965
+    c = 0.108103018168070
+    d = 0.091576213509771
+    omega1 = 0.109951743655322
+    omega2 = 0.223381589678011
+    para_pts = np.array([
+        (d, d),
+        (a, d),
+        (d, a),
+        (b, b),
+        (c, b),
+        (b, c)
+    ])
+    w = np.array([omega1, omega1, omega1, omega2, omega2, omega2])
+    """
+
+    f = []
+    for (xi, eta) in para_pts:
         e_xi = np.matmul(nodes, geo.dphi_dxi_quadratic(xi, eta, nodes))
         e_eta = np.matmul(nodes, geo.dphi_deta_quadratic(xi, eta, nodes))
-        normals[:, i] = np.cross(e_xi, e_eta)
-    return normals
+        n = np.cross(e_xi, e_eta)
+        if np.dot(n, tri_c - xc) < 0.:
+            print("reoriented normal")
+            n = -n
+        h_s = np.linalg.norm(n)
+        n_unit = n / h_s
+        f.append(func(xi, eta, n_unit, nodes) * h_s)
+    f = np.array(f)
+    f = np.transpose(np.array(f)) # make (3,7)
+
+    ret = 0.5 * np.dot(f, w)
+    return ret

rigid_DL/geometric.py
@@ -22,8 +22,8 @@ def pos_quadratic(xi, eta, nodes):
     position in a curved 6 node triangle as a function of eta and xi
 
     Parameters:
-        eta : parametric coordinate, scalar
         xi : paramteric coordinate, scalar
+        eta : parametric coordinate, scalar
         nodes : six nodes of triangle as columns in 3x6 ndarray
     Returns:
         x : output position (3,) ndarray
@@ -47,22 +47,23 @@ def dphi_dxi_quadratic(xi, eta, nodes):
     Calculates the dphi/dxi vector for a 2nd order curved triangular element.
 
     Parameters:
-        eta : parametric coordinate, scalar
         xi : paramteric coordinate, scalar
+        eta : parametric coordinate, scalar
         nodes : six nodes of triangle as columns in 3x6 ndarray
     Returns:
         dphi_dxi : dphi/dxi vector as ndarray shape (6)
     """
-    alpha, beta, gamma = calc_abg(nodes)
+    alpha, beta, gamma = calc_abg_alt(nodes)
+    (xi, eta) = (eta, xi)
     dphi_dxi = np.array([
         0.,
         (1. / (1. - alpha)) * (2. * xi - alpha + (alpha - gamma) / (1. - gamma) * eta),
-        (1. / (1. - beta)) * (beta + gamma - 1.) / (gamma) * eta,
+        (1. / (1. - beta)) * ((beta + gamma - 1.) / (gamma)) * eta,
         (1. / (alpha * (1. - alpha)) * (1. - 2. * xi - eta)),
         eta / (gamma * (1. - gamma)),
         -eta / (beta * (1. - beta)),
     ])
-    dphi_dxi[0] = 1. - np.sum(dphi_dxi)
+    dphi_dxi[0] = -np.sum(dphi_dxi)
     return dphi_dxi
 
 
@@ -71,22 +72,23 @@ def dphi_deta_quadratic(xi, eta, nodes):
     Calculates the dphi/deta vector for a 2nd order curved triangular element.
 
     Parameters:
-        eta : parametric coordinate, scalar
         xi : paramteric coordinate, scalar
+        eta : parametric coordinate, scalar
         nodes : six nodes of triangle as columns in 3x6 ndarray
     Returns:
         dphi_deta : dphi/deta vector as ndarray shape (6)
     """
-    alpha, beta, gamma = calc_abg(nodes)
+    alpha, beta, gamma = calc_abg_alt(nodes)
+    (xi, eta) = (eta, xi)
     dphi_deta = np.array([
         0.,
-        (1. / (1. - alpha)) * (alpha - gamma) / (1. - gamma) * xi,
-        (1. / (1. - beta)) * (2. * eta - beta + (beta + gamma - 1.) / (gamma) * xi),
+        (1. / (1. - alpha)) * ((alpha - gamma) / (1. - gamma)) * xi,
+        (1. / (1. - beta)) * (2. * eta - beta + ((beta + gamma - 1.) / (gamma)) * xi),
         -xi / (alpha * (1. - alpha)),
         xi / (gamma * (1. - gamma)),
         1. / (beta * (1. - beta)) * (1. - xi - 2. * eta),
     ])
-    dphi_deta[0] = 1. - np.sum(dphi_deta)
+    dphi_deta[0] = -np.sum(dphi_deta)
     return dphi_deta
 
 
@@ -219,3 +221,97 @@ def v_sph2cart(x, v):
     )
     dzdt = rd * math.cos(theta) - r * math.sin(theta) * td
     return np.array([dxdt, dydt, dzdt])
+
+
+### Alternative shape functions for testing ###
+def calc_abg_alt(nodes):
+    """
+    Calculate the alpha, beta, and gamma geometrical parameters for the parameterization
+    of a six-node curved triangle
+
+    Parameters:
+        nodes : six nodes of triangle as columns in 3x6 ndarray
+    Returns:
+        (alpha, beta, gamma) : tuple of floats
+    """
+    alpha = 1./(1 + np.linalg.norm(nodes[:, 5] - nodes[:, 0])/
+            np.linalg.norm(nodes[:, 5] - nodes[:, 2]))
+    beta = 1./(1 + np.linalg.norm(nodes[:, 4] - nodes[:, 1])/
+            np.linalg.norm(nodes[:, 4] - nodes[:, 2]))
+    gamma = 1./(1 + np.linalg.norm(nodes[:, 3] - nodes[:, 0])/
+            np.linalg.norm(nodes[:, 3] - nodes[:, 1]))
+    return (alpha, beta, gamma)
+
+
+def dphi_deta_quadratic_alt(xi, eta, nodes):
+    """
+    Calculates the dphi/deta vector for a 2nd order curved triangular element.
+
+    Parameters:
+        xi : paramteric coordinate, scalar
+        eta : parametric coordinate, scalar
+        nodes : six nodes of triangle as columns in 3x6 ndarray
+    Returns:
+        dphi_deta : dphi/deta vector as ndarray shape (6)
+    """
+    alpha, beta, gamma = calc_abg_alt(nodes)
+    dphi_deta = np.array([
+        alpha/(1-alpha) * ( 2 * eta/alpha  - 1 + (alpha - gamma)/(alpha * (1 - gamma) ) * xi ),
+        beta/(1-beta) * xi * ( (beta + gamma - 1)/(beta*gamma) ),
+        0.,
+        1/(gamma * (1 - gamma) ) * xi,
+        1/(beta * (1 - beta) ) * xi * -1,
+        1/(alpha * (1 - alpha) ) * (1 - 2 * eta - xi),
+    ])
+    dphi_deta[2] = -np.sum(dphi_deta)
+    return dphi_deta
+
+
+def pos_quadratic_alt(xi, eta, nodes):
+    """
+    position in a curved 6 node triangle as a function of eta and xi
+
+    Parameters:
+        eta : parametric coordinate, scalar
+        xi : paramteric coordinate, scalar
+        nodes : six nodes of triangle as columns in 3x6 ndarray
+    Returns:
+        x : output position (3,) ndarray
+    """
+    alpha, beta, gamma = calc_abg_alt(nodes)
+    phi = np.array([
+        alpha/(1-alpha) * eta * ( eta/alpha  - 1 + (alpha - gamma)/(alpha * (1 - gamma) ) * xi ),
+        beta/(1-beta) * xi * ( xi/beta  - 1 + (beta + gamma - 1)/(beta*gamma) * eta ),
+        0.,
+        1/(gamma * (1 - gamma) ) * eta * xi,
+        1/(beta * (1 - beta) ) * xi * (1 - eta - xi),
+        1/(alpha * (1 - alpha) ) * eta * (1 - eta - xi),
+    ])
+    phi[2] = 1. - np.sum(phi)
+
+    x = np.matmul(nodes, phi)
+    return x
+
+
+def dphi_dxi_quadratic_alt(xi, eta, nodes):
+    """
+    Calculates the dphi/dxi vector for a 2nd order curved triangular element.
+
+    Parameters:
+        xi : paramteric coordinate, scalar
+        eta : parametric coordinate, scalar
+        nodes : six nodes of triangle as columns in 3x6 ndarray
+    Returns:
+        dphi_dxi : dphi/dxi vector as ndarray shape (6)
+    """
+    alpha, beta, gamma = calc_abg_alt(nodes)
+    dphi_dxi = np.array([
+        alpha/(1-alpha) * eta * ( (alpha - gamma)/(alpha * (1 - gamma) ) ),
+        beta/(1-beta) * ( 2 * xi/beta  - 1 + (beta + gamma - 1)/(beta*gamma) * eta ),
+        0.,
+        1/(gamma * (1 - gamma) ) * eta,
+        1/(beta * (1 - beta) ) * (1 - eta - 2 * xi),
+        1/(alpha * (1 - alpha) ) * eta * -1,
+    ])
+    dphi_dxi[2] = -np.sum(dphi_dxi)
+    return dphi_dxi

rigid_DL/lin_mesh_test.py
@@ -47,9 +47,9 @@ def main():
 
     w, v = np.linalg.eig(C)
     io.write_eigval(w, "{}_eigval.csv".format(out_name))
-    io.write_eigvec(v, "{}_eigvec.csv".format(out_name))
-    io.write_vel(v_trans_in, v_trans_out, "{}_trans_vel.csv".format(out_name))
-    io.write_vel(v_12_in, v_12_out, "{}_12_vel.csv".format(out_name))
+    #io.write_eigvec(v, "{}_eigvec.csv".format(out_name))
+    #io.write_vel(v_trans_in, v_trans_out, "{}_trans_vel.csv".format(out_name))
+    #io.write_vel(v_12_in, v_12_out, "{}_12_vel.csv".format(out_name))
 
     t3 = time.time()
     print("{}, write out walltime".format(t3 - t2))

rigid_DL/mat_assembly.py
@@ -138,7 +138,7 @@ def make_mat_cp_qe(quad_mesh):
                 C[(3 * src_num):(3 * src_num + 3),
                   (3 * face_num):(3 * face_num + 3)] += sub_mat
                 C[(3 * src_num):(3 * src_num + 3),
-                  (3 * src_num):(3 * src_num + 3)] -= sub_mat
+                 (3 * src_num):(3 * src_num + 3)] -= sub_mat
             # do nothing face_num == src_num, how it works out for constant elements
 
     for src_num in range(num_faces):
@@ -330,3 +330,69 @@ def make_sing_lp_qe_quad_func(x_0, node_num, singular_ind):
             else:
                 return (phi) * geo.stresslet(x, x_0)
     return quad_func
+
+
+### Alternative versions for testing ###
+def make_cp_qe_quad_func_alt(x_0):
+    """"
+    Makes the constant potential function that is integrated over
+    quadratic elements for the stiffness matrix
+    Version that makes function that takes in normal vector
+
+    Parameters:
+        x_0: the source point
+    """
+    def quad_func(xi, eta, n, nodes):
+        x = geo.pos_quadratic(xi, eta, nodes)
+        return geo.stresslet_n(x, x_0, n)
+    return quad_func
+
+
+def make_cp_qe_quad_func_alt2(x_0):
+    """"
+    Makes the constant potential function that is integrated over
+    quadratic elements for the stiffness matrix
+    Version that makes function that takes in normal vector
+
+    Parameters:
+        x_0: the source point
+    """
+    def quad_func(xi, eta, n, nodes):
+        x = geo.pos_linear(xi, eta, nodes[:,0:3])
+        return geo.stresslet_n(x, x_0, n)
+    return quad_func
+
+def make_mat_cp_qe_alt(quad_mesh):
+    """
+    Slow version for debugging, did not fix the problem.
+
+    Parameters:
+        quad_mesh : simple quadratic mesh input
+    Returns:
+        the stresslet matrix
+    """
+    num_faces = quad_mesh.faces.shape[0]
+    c_0 = 1. / (4. * np.pi)
+    C = np.zeros((3 * num_faces, 3 * num_faces))
+    for src_num in range(num_faces): # source points
+        src_center = quad_mesh.calc_tri_center(quad_mesh.get_nodes(quad_mesh.faces[src_num]))
+        for face_num in range(num_faces): # field points
+            face_nodes = quad_mesh.get_nodes(quad_mesh.faces[face_num])
+            if face_num != src_num:
+                sub_mat = gq.int_over_tri_quad_slow(
+                    make_cp_qe_quad_func_alt(src_center),
+                    face_nodes,
+                    quad_mesh.centroid,
+                    quad_mesh.calc_tri_center(face_nodes)
+                )
+                C[(3 * src_num):(3 * src_num + 3),
+                  (3 * face_num):(3 * face_num + 3)] += sub_mat
+                C[(3 * src_num):(3 * src_num + 3),
+                  (3 * src_num):(3 * src_num + 3)] -= sub_mat
+            # do nothing face_num == src_num, how it works out for constant elements
+        C[(3 * src_num):(3 * src_num + 3),
+          (3 * src_num):(3 * src_num + 3)] -= 4. * np.pi * np.identity(3)
+    C = np.dot(c_0, C)
+    return C
+
+

rigid_DL/quad_mesh_test.py
@@ -24,10 +24,8 @@ def main():
             faces = cell_block.data
     t1 = time.time()
     print("{}, meshio read time".format(t1 - t0))
-
     quad_mesh = sqm.simple_quad_mesh(verts, faces)
     print("Calculated mesh dims: {}".format(quad_mesh.dims))
-
     C = mata.make_mat_cp_qe(quad_mesh)
 
     v_cnst = np.array([1, 0, 0])
@@ -46,9 +44,9 @@ def main():
 
     w, v = np.linalg.eig(C)
     io.write_eigval(w, "quad_test_eigval.csv")
-    io.write_eigvec(v, "quad_test_eigvec.csv")
-    io.write_vel(v_trans_in, v_trans_out, "quad_test_trans_vel.csv")
-    io.write_vel(v_12_in, v_12_out, "quad_test_E12_vel.csv")
+    #io.write_eigvec(v, "quad_test_eigvec.csv")
+    #io.write_vel(v_trans_in, v_trans_out, "quad_test_trans_vel.csv")
+    #io.write_vel(v_12_in, v_12_out, "quad_test_E12_vel.csv")
     t1 = time.time()
     print("{}, write out walltime".format(t1 - t0))
 

rigid_DL/simple_quad_mesh.py
@@ -26,13 +26,12 @@ class simple_quad_mesh:
         self.lin_verts = None
         self.lin_faces = None
         self.make_linear_mesh()
+        self.centroid = self.calc_mesh_centroid_pc()
         self.quad_n = self.calc_all_quad_n()
         self.quad_hs = self.calc_all_quad_hs()
         self.surf_area = self.calc_surf_area()
-        self.centroid = self.calc_mesh_centroid()
         self.mom_inertia = self.calc_moment_inertia_tensor()
         self.dims = self.calc_ellip_dims()
-        print(self.centroid)
 
 
     def make_linear_mesh(self):
@@ -77,7 +76,8 @@ class simple_quad_mesh:
         normals = np.empty([Nf, 3, 7])
         for i, face in enumerate(self.faces):
             nodes = self.get_nodes(face)
-            normals[i] = gq.quad_n(nodes)
+            tri_c = self.calc_tri_center(nodes)
+            normals[i] = gq.quad_n(nodes, self.centroid, tri_c)
         return normals
 
 
@@ -120,6 +120,14 @@ class simple_quad_mesh:
             nodes = self.get_nodes(face)
             x_c += gq.int_over_tri_quad(geo.pos_quadratic, nodes, self.quad_hs[i])
         x_c /= self.surf_area
+        return x_c
+
+
+    def calc_mesh_centroid_pc(self):
+        """
+        Centroid of mesh assuming point cloud
+        """
+        x_c = np.mean(self.vertices, axis=0)
         return x_c
 
 
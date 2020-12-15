Fixed sign error and w_m calculation * w_m calculation fixed to use eigenvectors of moment of inertia tensor * A_m calculation uses eigenvalues of moment of inertia tensor * See calculating w_m notes for explaination of why moment of inertia tensor * Fixed sign error in rotation CDL terms, came from weirdness in np.einsum(), split to two operations, np.outer(np.einsum()) * Alternative version for RBM cp_le RBM terms using np.cross() added * lp_le RBM terms also editted

rigid_DL/lin_geo_mesh.py
@@ -13,7 +13,6 @@ class Lin_Geo_Mesh(Geo_Mesh):
     def __init__(self, x, f):
         """
         Constructor for the simple_mesh object.
-
         Parameters:
             x : verts in list-like object (Nv, 3)
             f : list-like with indices to verts of a triangle
@@ -30,6 +29,11 @@ class Lin_Geo_Mesh(Geo_Mesh):
         self.normalize_n() # normal vectors normalized
         self.mom_inertia = self.calc_moment_inertia_tensor()
         self.dims = self.calc_ellip_dims()
+        (self.w, self.A_m) = self.calc_rotation_eig()
+        print("lin_geo w:")
+        print(self.w)
+        print("lin_geo A_m:")
+        print(self.A_m)
 
 
     def get_verts(self):
@@ -51,7 +55,6 @@ class Lin_Geo_Mesh(Geo_Mesh):
     def get_tri_nodes(self, face_num):
         """
         Gets the nodes of a face and puts nodes into a (3, 3) matrix
-
         Paramters:
             face_num : face number
         Returns:
@@ -68,7 +71,6 @@ class Lin_Geo_Mesh(Geo_Mesh):
     def get_tri_center(self, face_num):
         """
         Calculates the centroid point on a face
-
         Parameters:
             face_num : face number
         Returns:
@@ -82,7 +84,6 @@ class Lin_Geo_Mesh(Geo_Mesh):
     def get_normal(self, face_num):
         """
         Return normal vector for face.
-
         Parameters:
             face_num: face numbers
         Returns:
@@ -94,13 +95,26 @@ class Lin_Geo_Mesh(Geo_Mesh):
     def get_hs(self, face_num):
         """
         Return hs value for face.
-
         Parameters:
             face_num: face numbers
         Returns:
             hs: scalar
         """
         return self.hs[face_num]
+
+
+    def get_w(self):
+        """
+        Return rotation vectors for rotation eigenfunction
+        """
+        return self.w
+
+
+    def get_A_m(self):
+        """
+        Return eigenvalues of moment of inertia tensor
+        """
+        return self.A_m
 
 
     def calc_all_n(self):
@@ -163,21 +177,20 @@ class Lin_Geo_Mesh(Geo_Mesh):
         for i, face in enumerate(self.faces):
             nodes = self.get_tri_nodes(i)
             inertia_tensor += gq.int_over_tri_lin(geo.inertia_func_linear, nodes, self.hs[i])
-        inertia_tensor /= self.surf_area
         return inertia_tensor
 
 
-    def calc_rotation_vectors(self):
+    def calc_rotation_eig(self):
         """
         Calculates the rotation vectors (eigenvectors of moment of inertia)
         Be careful of sphere case when this basis is no longer orthogonal.
-        Pretty sure can just use cartesional unit vectors for every case.
         """
-        if self.is_sphere:
-            return np.identity(3)
         eig_vals, eig_vecs = np.linalg.eig(self.mom_inertia)
+        idx = eig_vals.argsort()
+        eig_vals = eig_vals[idx]
+        eig_vecs = eig_vecs[:, idx]
         w = eig_vecs.T
-        return w
+        return (w, eig_vals)
 
 
     def calc_ellip_dims(self):

rigid_DL/mobil_assembly.py
@@ -74,20 +74,18 @@ def add_cp_le_RBM_terms(K, cons_pot_mesh, lin_geo_mesh):
     """
     num_faces = cons_pot_mesh.get_faces().shape[0]
     x_c = lin_geo_mesh.get_centroid()
-    w = np.identity(3)
-    A_m = mobil_helper.calc_le_Am_vec(lin_geo_mesh)
-    print("A_m:")
-    print(A_m)
+    w = lin_geo_mesh.get_w()
+    A_m = lin_geo_mesh.get_A_m()
     S_D = lin_geo_mesh.get_surface_area()
 
     for face_num in range(num_faces):
         face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
         face_hs = lin_geo_mesh.get_hs(face_num)
-        v_sub_mat = (-4. * np.pi / S_D) * (np.identity(3) * 0.5 * face_hs) # simple integral
+        v_sub_mat = (1. / S_D) * (np.identity(3) * 0.5 * face_hs) # simple integral
         def omega_quad(xi, eta, nodes):
             pos = geo.pos_linear(xi, eta, nodes)
             X = pos - x_c
-            return np.einsum("lrs,r", geo.LC_3, X)
+            return np.einsum("lrs,s->lr", geo.LC_3, X)
         tmp_omega = gq.int_over_tri_lin(
             omega_quad,
             face_nodes,
@@ -95,16 +93,68 @@ def add_cp_le_RBM_terms(K, cons_pot_mesh, lin_geo_mesh):
         )
         tmp_arr = []
         for m in range(3):
-            tmp_arr.append((1./ A_m[m]) * np.einsum("j,l,ls", w[m], w[m], tmp_omega))
-        tmp_omega_mat = -4. * np.pi * np.sum(tmp_arr, axis=0)
+            #not sure why, but this is negative of what is needed
+            #tmp_arr.append((1./ A_m[m]) * np.einsum("j,l,ls->js", w[m], w[m], tmp_omega))
+            tmp_arr.append((1./ A_m[m]) * np.outer(w[m], np.einsum("l,ls", w[m], tmp_omega)))
+        tmp_arr = np.array(tmp_arr)
+        tmp_omega_mat = np.sum(tmp_arr, axis=0)
         for src_num in range(num_faces):
             K[(3 * src_num):(3 * src_num + 3),
-              (3 * face_num):(3 * face_num + 3)] += -1. / (4. * np.pi) * v_sub_mat
+              (3 * face_num):(3 * face_num + 3)] += v_sub_mat
             src_center = cons_pot_mesh.get_node(src_num)
             X_0 = src_center - x_c
             omega_mat = np.einsum("ijk,js,k->is", geo.LC_3, tmp_omega_mat, X_0)
             K[(3 * src_num):(3 * src_num + 3),
-              (3 * face_num):(3 * face_num + 3)] += -1. / (4. * np.pi) * omega_mat # error?
+              (3 * face_num):(3 * face_num + 3)] += omega_mat # sign error?
+
+
+def add_cp_le_RBM_terms_alt(K, cons_pot_mesh, lin_geo_mesh):
+    """
+    Alternative using np.cross()
+    Add rigid body motion terms to the given stiffness matrix K.
+    For constant potential and linear elements.
+    Parameters:
+        K: stiffness matrix to add terms to (3 * face_num, 3 * face_num) ndarray
+        cons_pot_mesh: constant potential mesh
+        lin_geo_mesh : linear geometric mesh
+    Returns:
+        None
+    """
+    num_faces = cons_pot_mesh.get_faces().shape[0]
+    x_c = lin_geo_mesh.get_centroid()
+    w = lin_geo_mesh.get_w()
+    A_m = lin_geo_mesh.get_A_m()
+    S_D = lin_geo_mesh.get_surface_area()
+
+    for face_num in range(num_faces):
+        face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
+        face_hs = lin_geo_mesh.get_hs(face_num)
+        v_sub_mat = (1. / S_D) * (np.identity(3) * 0.5 * face_hs) # simple integral
+        tmp_omega = []
+        for m in range(3):
+            def make_omega_quad(w, A_m):
+                def omega_quad(xi, eta, nodes):
+                    pos = geo.pos_linear(xi, eta, nodes)
+                    X = pos - x_c
+                    return np.cross(w, X)
+                return omega_quad
+            tmp_omega.append(gq.int_over_tri_lin(
+                make_omega_quad(w[m], A_m[m]),
+                face_nodes,
+                face_hs,
+            ))
+        for src_num in range(num_faces):
+            K[(3 * src_num):(3 * src_num + 3),
+              (3 * face_num):(3 * face_num + 3)] += v_sub_mat
+            src_center = cons_pot_mesh.get_node(src_num)
+            X_0 = src_center - x_c
+            tmp_arr = []
+            for m in range(3):
+                tmp_arr.append(np.outer((1./ A_m[m]) * np.cross(w[m], X_0), tmp_omega[m]))
+            tmp_arr = np.array(tmp_arr)
+            omega_mat = np.sum(tmp_arr, axis=0)
+            K[(3 * src_num):(3 * src_num + 3),
+              (3 * face_num):(3 * face_num + 3)] += omega_mat
 
 
 def add_cp_le_n_terms(K, cons_pot_mesh, lin_geo_mesh):
@@ -326,8 +376,8 @@ def add_lp_le_RBM_terms(K, lin_pot_mesh, lin_geo_mesh):
     num_nodes = pot_nodes.shape[0]
     S_D = lin_geo_mesh.get_surface_area()
     x_c = lin_geo_mesh.get_centroid()
-    w = np.identity(3)
-    A_m = mobil_helper.calc_le_Am_vec(lin_geo_mesh)
+    w = lin_geo_mesh.get_w()
+    A_m = lin_geo_mesh.get_A_m()
 
     for face_num in range(num_faces):
         face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
@@ -347,7 +397,7 @@ def add_lp_le_RBM_terms(K, lin_pot_mesh, lin_geo_mesh):
             )
             tmp_arr = []
             for m in range(3):
-                tmp_arr.append((1./ A_m[m]) * np.einsum("j,l,ls", w[m], w[m], tmp_omega))
+                tmp_arr.append((1./ A_m[m]) * np.outer(w[m], np.einsum("l, ls", w[m], tmp_omega)))
             tmp_omega_mat = -4. * np.pi * np.sum(tmp_arr, axis=0)
             for src_num in range(num_nodes):
                 K[(3 * src_num):(3 * src_num + 3),
@@ -356,7 +406,7 @@ def add_lp_le_RBM_terms(K, lin_pot_mesh, lin_geo_mesh):
                 X_0 = src_pt - x_c
                 omega_mat = np.einsum("ijk,js,k", geo.LC_3, tmp_omega_mat, X_0)
                 K[(3 * src_num):(3 * src_num + 3),
-                  j:j+3] += 1. / (4. * np.pi) * (omega_mat) #error?
+                  j:j+3] += 1. / (4. * np.pi) * (omega_mat)
 
 
 def make_DL_reg_lp_le_quad_func(n, x_0, node_num):

rigid_DL/mobil_helper.py
@@ -40,9 +40,9 @@ def calc_cp_le_rot_vel(lin_geo_mesh, q):
     """
     num_faces = lin_geo_mesh.get_faces().shape[0]
     x_c = lin_geo_mesh.get_centroid()
-    w = np.identity(3)
+    w = lin_geo_mesh.get_w()
+    A_m = lin_geo_mesh.get_A_m()
     omega_vec = 0.
-    A_m = calc_le_Am_vec(lin_geo_mesh)
     for face_num in range(num_faces):
         face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
         face_hs = lin_geo_mesh.get_hs(face_num)
@@ -121,9 +121,9 @@ def calc_lp_le_rot_vel(lin_pot_mesh, lin_geo_mesh, q):
     num_faces = lin_pot_mesh.get_faces().shape[0]
     pot_faces = lin_pot_mesh.get_faces()
     x_c = lin_geo_mesh.get_centroid()
-    w = np.identity(3)
+    w = lin_geo_mesh.get_w()
+    A_m = lin_geo_mesh.get_A_m()
     omega_vec = 0.
-    A_m = calc_le_Am_vec(lin_geo_mesh)
     for face_num in range(num_faces):
         face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
         face_hs = lin_geo_mesh.get_hs(face_num)
@@ -158,6 +158,7 @@ def calc_lp_le_rot_vel(lin_pot_mesh, lin_geo_mesh, q):
 
 def calc_le_Am_vec(lin_geo_mesh):
     """
+    Deprecated, version calculated from moment of inertia tensor better.
     Calculate the A_m vector given a linear geometric mesh.
     Parameters:
         lin_geo_mesh: linear geometric mesh

rigid_DL/test_mobil_assem.py
@@ -34,9 +34,9 @@ for cell_block in io_mesh.cells:
         faces = cell_block.data
         break
 
+dims = args.dims
 geo_mesh = lin_geo_mesh.Lin_Geo_Mesh(verts, faces)
 mu = args.mu
-dims = args.dims
 print("Dims:")
 print(dims)
 E_d, E_c = eig_funs.E_12(geo_mesh, dims)
@@ -81,7 +81,13 @@ print(np.linalg.norm(tmp_1 - tmp_0) / np.linalg.norm(tmp_0))
 pot_mesh = lin_pot_mesh.Lin_Pot_Mesh.make_from_lin_geo_mesh(geo_mesh)
 num_nodes = pot_mesh.get_nodes().shape[0]
 K = m_a.make_mat_lp_le(pot_mesh, geo_mesh)
-eig_vals, eig_vecs = np.linalg.eig(K + np.identity(3 * num_nodes))
+eig_vals, eig_vecs = np.linalg.eig(K)
+
+out_file = "{}_lp_le_eig.txt".format(args.out_tag)
+with open(out_file, 'w') as out:
+    out.write("eigenvalues\n")
+    for a in sorted(eig_vals.real):
+        out.write("{}\n".format(a))
 
 u_d = eig_helper.make_lin_eig_vels(pot_mesh, E_d, E_c)
 
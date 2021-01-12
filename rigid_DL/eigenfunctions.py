Quadratic mesh mobility problem fix and rotation error progress 1. Fixes cp_qe and lp_qe mobility matrix assembly. Previous versions did not edit the K matrix in place and rather made a new local version 2. Tested several possible origins for the rotation error that leads to a plateau'ing of total relative error.     a. moment of inertia calculation: is correct but slightly off for     quadratic meshes     b. eigenvalue calculation: code produces same answers as simplified     calculation for prolate spheroids as seen in Kim and Karrilla     c. eigenvector calculation: the calculation is coded correctly     d. different discretization (Gmsh): total relative error also     plateaus but at a different value?     e. lower aspect ratios: (1.2)11 ellipsoid has lower plateau value     ~3.5% rather than ~9% seen for 411.

rigid_DL/eigenfunctions.py
@@ -19,21 +19,21 @@ def E_12(dims):
     """
     E_d = np.array([[0., 1., 0.], [1., 0., 0.], [0., 0., 0.]])
     a, b, _c = dims
-    E_c = (a**2 - b**2)/(a**2 + b**2) * E_d[0, 1] * np.array([0., 0., 1.])
+    E_c = ((a**2 - b**2)/(a**2 + b**2)) * E_d[0, 1] * np.array([0., 0., 1.])
     return (E_d, E_c)
 
 
 def E_31(dims):
     E_d = np.array([[0., 0., 1.], [0., 0., 0.], [1., 0., 0.]])
     a, _b, c = dims
-    E_c = (c**2 - a**2)/(c**2 + a**2) * E_d[0, 2] * np.array([0., 1., 0.])
+    E_c = ((c**2 - a**2)/(c**2 + a**2)) * E_d[0, 2] * np.array([0., 1., 0.])
     return (E_d, E_c)
 
 
 def E_23(dims):
     E_d = np.array([[0., 0., 0.], [0., 0., 1.], [0., 1., 0.]])
     _a, b, c = dims
-    E_c = (b**2 - c**2)/(b**2 + c**2) * E_d[1, 2] * np.array([1., 0., 0.])
+    E_c = ((b**2 - c**2)/(b**2 + c**2)) * E_d[1, 2] * np.array([1., 0., 0.])
     return (E_d, E_c)
 
 

rigid_DL/gauss_quad.py
@@ -87,11 +87,10 @@ def int_over_tri_quad_n(func, nodes, n):
     return ret
 
 
-def quad_n(nodes, xc, tri_c):
+def quad_n(nodes):
     """
     Calculate the normal vector values over a curved triangle for six point Gaussian quadrature.
     This function is use to reduce the number of times these values are recalculated.
-    Reorients the normal if needed.
 
     Parameters:
         nodes: 3x6 ndarray with nodes as column vectors
@@ -103,9 +102,6 @@ def quad_n(nodes, xc, tri_c):
         e_xi = np.einsum("ij,j->i", nodes, geo.dphi_dxi_quadratic(xi, eta, nodes))
         e_eta = np.einsum("ij,j->i", nodes, geo.dphi_deta_quadratic(xi, eta, nodes))
         n = np.cross(e_xi, e_eta)
-        if np.dot(n, tri_c - xc) < 0.:
-            print("n reori")
-            n = -n
         normals[:, i] = n
     return normals
 

rigid_DL/mobil_assembly.py
@@ -76,10 +76,6 @@ def add_cp_le_RBM_terms(K, cons_pot_mesh, lin_geo_mesh):
     x_c = lin_geo_mesh.get_centroid()
     w = lin_geo_mesh.get_w()
     A_m = lin_geo_mesh.get_A_m()
-    print("Original eigenvalue of M:")
-    print(A_m)
-    print("Other method:")
-    print(mobil_helper.calc_le_Am_vec(lin_geo_mesh))
     S_D = lin_geo_mesh.get_surface_area()
 
     for face_num in range(num_faces):
@@ -240,7 +236,7 @@ def make_cp_le_forcing_vec_SS(cons_pot_mesh, lin_geo_mesh, u_d, f, l, mu):
 
 def make_forcing_vec(pot_mesh, geo_mesh, u_d, f, l, mu):
     """
-    Makes the forcing vector ( f ) for the mobility problem 
+    Makes the forcing vector ( f ) for the mobility problem
     For the direct solution method.
     Parameters:
         pot_mesh: potential mesh
@@ -479,7 +475,6 @@ def add_cp_qe_DL_terms(K, cons_pot_mesh, quad_geo_mesh):
     assert geo_faces.shape[0] == pot_faces.shape[0]
     num_faces = geo_faces.shape[0]
     c_0 = 1. / (4. * np.pi)
-    K = np.zeros((3 * num_faces, 3 * num_faces))
     for face_num in range(num_faces): # field points
         face_nodes = quad_geo_mesh.get_tri_nodes(face_num)
         face_n = quad_geo_mesh.get_quad_n(face_num)
@@ -500,8 +495,7 @@ def add_cp_qe_DL_terms(K, cons_pot_mesh, quad_geo_mesh):
     for src_num in range(num_faces):
         K[(3 * src_num):(3 * src_num + 3),
           (3 * src_num):(3 * src_num + 3)] -= 4. * np.pi * np.identity(3)
-    K = np.dot(c_0, K)
-    return K
+    K *= c_0
 
 
 def make_cp_qe_quad_func(x_0):
@@ -562,3 +556,182 @@ def add_cp_qe_RBM_terms(K, cons_pot_mesh, quad_geo_mesh):
             omega_mat = np.einsum("ijk,js,k->is", geo.LC_3, tmp_omega_mat, X_0)
             K[(3 * src_num):(3 * src_num + 3),
               (3 * face_num):(3 * face_num + 3)] += omega_mat
+
+
+def make_mat_lp_qe(lin_pot_mesh, quad_geo_mesh):
+    """
+    Mobility problem.
+    Makes the stiffness matrix using closed surface singularity subtraction.
+    For linear potentials over quadratic elements.
+    Parameters:
+        lin_pot_mesh: linear potential mesh
+        quad_geo_mesh: quadratic geometric mesh
+    Returns:
+        the stresslet matrix
+    """
+    num_nodes = lin_pot_mesh.get_nodes().shape[0]
+    K = np.zeros((3 * num_nodes, 3 * num_nodes))
+    add_lp_qe_DL_terms(K, lin_pot_mesh, quad_geo_mesh)
+    add_lp_qe_RBM_terms(K, lin_pot_mesh, quad_geo_mesh)
+    return K
+
+
+def add_lp_qe_DL_terms(K, lin_pot_mesh, quad_geo_mesh):
+    """
+    Make DL terms for linear potential, quadratic elements mesh
+    $-\frac{1}{4\pi} \Int{q_j(\bm{x}) T_{jik}(\bm{x}, \bm{x}_0) n_k(\bm{x})}{S(\bm{x}), D, PV}$
+    Parameters:
+        K: stiffness matrix to add terms to (3 * face_num, 3 * face_num) ndarray
+        lin_pot_mesh: linear potential mesh
+        quad_geo_mesh : quadratic geometric mesh
+    Returns:
+        None
+    """
+    geo_faces = quad_geo_mesh.get_faces()
+    pot_faces = lin_pot_mesh.get_faces()
+    assert geo_faces.shape[0] == pot_faces.shape[0]
+    num_faces = geo_faces.shape[0]
+    pot_nodes = lin_pot_mesh.get_nodes()
+    num_nodes = pot_nodes.shape[0]
+    c_0 = 1. / (4. * np.pi)
+
+    for face_num in range(num_faces): # integrate over faces
+        face_nodes = quad_geo_mesh.get_tri_nodes(face_num)
+        face_n = quad_geo_mesh.quad_n[face_num]
+        for src_num in range(num_nodes): # source points
+            src_pt = lin_pot_mesh.get_node(src_num)
+            is_singular, local_singular_ind = lin_pot_mesh.check_in_face(src_num, face_num)
+
+            if is_singular: # singular triangle
+                for node_num in range(3):
+                    node_global_num = pot_faces[face_num, node_num] # global index for vert
+                    sub_mat = gq.int_over_tri_quad_n(
+                        make_sing_lp_qe_quad_func(
+                            src_pt, node_num, local_singular_ind
+                            ),
+                        face_nodes,
+                        face_n
+                    )
+                    K[(3 * src_num):(3 * src_num + 3),
+                      (3 * node_global_num):(3 * node_global_num + 3)] += sub_mat
+
+            else: # regular triangle
+                for node_num in range(3):
+                    node_global_num = pot_faces[face_num, node_num] # global index for vert
+                    sub_mat = gq.int_over_tri_quad_n(
+                        make_reg_lp_qe_quad_func(src_pt, node_num),
+                        face_nodes,
+                        face_n
+                    )
+                    K[(3 * src_num):(3 * src_num + 3),
+                      (3 * node_global_num):(3 * node_global_num + 3)] += sub_mat
+                # subtracting the q(x_0) term
+                sub_mat = gq.int_over_tri_quad_n(
+                    make_cp_qe_quad_func(src_pt),
+                    face_nodes,
+                    face_n
+                )
+                K[(3 * src_num):(3 * src_num + 3), (3 * src_num):(3 * src_num + 3)] -= sub_mat
+
+    for src_num in range(num_nodes): # source points
+        # whole surface q(x_0) term
+        K[(3 * src_num):(3 * src_num + 3), (3 * src_num):(3 * src_num + 3)] -= (
+            4. * np.pi * np.identity(3)
+        )
+
+    K *= c_0
+
+
+def make_reg_lp_qe_quad_func(x_0, node_num):
+    """
+    Makes the regular (non-singular) linear potential, quadratic element function
+    that is integrated for the stiffness matrix
+    Parameters:
+        x_0: source point
+        node_num: which potential shape function [0, 1, 2]
+    """
+    def quad_func(xi, eta, nodes):
+        x = geo.pos_quadratic(xi, eta, nodes)
+        S = geo.stresslet(x, x_0)
+        phi = geo.shape_func_linear(xi, eta, node_num)
+        return phi * S
+    return quad_func
+
+
+def make_sing_lp_qe_quad_func(x_0, node_num, singular_ind):
+    """
+    Makes the sinuglar linear potential, quadratic element function
+    that is dotted with normal vectors and integrated for the stiffness matrix
+    Parameters:
+        x_0: source point
+        node_num: which potential shape function [0, 1, 2]
+        singular_ind: local singular index for a face
+    """
+    def quad_func(xi, eta, nodes):
+        x = geo.pos_quadratic(xi, eta, nodes)
+        phi = geo.shape_func_linear(xi, eta, node_num)
+        if node_num == singular_ind:
+            if (phi - 1) == 0: # getting around division by 0
+                return np.zeros([3, 3, 3])
+            else:
+                if np.linalg.norm(x - x_0) < 1e-6:
+                    print("nearly singular lp qe x_hat error")
+                return (phi - 1) * geo.stresslet(x, x_0)
+        else:
+            if phi == 0:
+                return np.zeros([3, 3, 3])
+            else:
+                if np.linalg.norm(x - x_0) < 1e-6:
+                    print("nearly singular lp qe x_hat error")
+                return (phi) * geo.stresslet(x, x_0)
+    return quad_func
+
+
+def add_lp_qe_RBM_terms(K, lin_pot_mesh, quad_geo_mesh):
+    """
+    Add rigid body motion terms to the given stiffness matrix K.
+    For linear potential and quadratic elements.
+    Parameters:
+        K: stiffness matrix to add terms to (3 * node_num, 3 * node_num) ndarray
+        lin_pot_mesh: constant potential mesh
+        quad_geo_mesh: quadratic geometric mesh
+    Returns:
+        None
+    """
+    pot_faces = lin_pot_mesh.get_faces()
+    num_faces = pot_faces.shape[0]
+    pot_nodes = lin_pot_mesh.get_nodes()
+    num_nodes = pot_nodes.shape[0]
+    S_D = quad_geo_mesh.get_surface_area()
+    x_c = quad_geo_mesh.get_centroid()
+    w = quad_geo_mesh.get_w()
+    A_m = quad_geo_mesh.get_A_m()
+
+    for face_num in range(num_faces):
+        face_nodes = quad_geo_mesh.get_tri_nodes(face_num)
+        face_hs = quad_geo_mesh.get_hs(face_num)
+        for node_num in range(3): # face nodes
+            face_node_global_num = pot_faces[face_num, node_num]
+            v_sub_mat = (-4. * np.pi / S_D) * gq.int_over_tri_quad(
+                make_lp_le_v_quad(node_num),
+                face_nodes,
+                face_hs,
+            )
+            j = 3 * face_node_global_num
+            tmp_omega = gq.int_over_tri_quad(
+                make_lp_le_omega_quad(node_num, x_c),
+                face_nodes,
+                face_hs,
+            )
+            tmp_arr = []
+            for m in range(3):
+                tmp_arr.append((1./ A_m[m]) * np.outer(w[m], np.einsum("l, ls", w[m], tmp_omega)))
+            tmp_omega_mat = -4. * np.pi * np.sum(tmp_arr, axis=0)
+            for src_num in range(num_nodes):
+                K[(3 * src_num):(3 * src_num + 3),
+                  j:j+3] += -1. / (4. * np.pi) * v_sub_mat
+                src_pt = lin_pot_mesh.get_node(src_num)
+                X_0 = src_pt - x_c
+                omega_mat = np.einsum("ijk,js,k", geo.LC_3, tmp_omega_mat, X_0)
+                K[(3 * src_num):(3 * src_num + 3),
+                  j:j+3] += 1. / (4. * np.pi) * (omega_mat)

rigid_DL/mobil_helper.py
@@ -65,6 +65,48 @@ def calc_cp_le_rot_vel(lin_geo_mesh, q):
     return omega_vec
 
 
+def calc_lp_le_trans_vel(lin_pot_mesh, lin_geo_mesh, q):
+    """
+    Calculate the translational velocity of a particle.
+    Linear potential, linear elements
+    Parameters:
+        lin_pot_mesh: linear potential mesh
+        lin_geo_mesh: geometric mesh
+        q: DL potential solution; (3 * num_nodes,) ndarray
+    Returns:
+        part_v: particle translational velocity; (3,) ndarray
+    """
+    part_v = np.zeros(3)
+    pot_faces = lin_pot_mesh.get_faces()
+    num_faces = pot_faces.shape[0]
+    S_D = lin_geo_mesh.get_surface_area()
+    for face_num in range(num_faces):
+        face_nodes = lin_geo_mesh.get_tri_nodes(face_num)
+        face_hs = lin_geo_mesh.get_hs(face_num)
+        def make_v_sol_quad(face_num):
+            def v_quad(xi, eta, _nodes):
+                node_0 = pot_faces[face_num, 0]
+                node_1 = pot_faces[face_num, 1]
+                node_2 = pot_faces[face_num, 2]
+                phi_0 = geo.shape_func_linear(xi, eta, 0)
+                phi_1 = geo.shape_func_linear(xi, eta, 1)
+                phi_2 = geo.shape_func_linear(xi, eta, 2)
+                ret = (
+                    q[3 * node_0 : 3 * node_0 + 3] * phi_0 +
+                    q[3 * node_1 : 3 * node_1 + 3] * phi_1 +
+                    q[3 * node_2 : 3 * node_2 + 3] * phi_2
+                )
+                return ret
+            return v_quad
+        part_v += gq.int_over_tri_lin(
+            make_v_sol_quad(face_num),
+            face_nodes,
+            face_hs,
+        )
+    part_v *= (-4. * np.pi) / S_D
+    return part_v
+
+
 def calc_lp_le_rot_vel(lin_pot_mesh, lin_geo_mesh, q):
     """
     Calculate the rotational velocity of a particle.
@@ -132,7 +174,7 @@ def calc_cp_qe_trans_vel(quad_geo_mesh, q):
         face_hs = quad_geo_mesh.get_hs(face_num)
         j = 3 * face_num
         def make_v_quad(q_vec):
-            def v_quad(xi, eta, nodes):
+            def v_quad(_xi, _eta, _nodes):
                 return q_vec
             return v_quad
         part_v += gq.int_over_tri_quad(make_v_quad(q[j : j+3]), face_nodes, face_hs)
@@ -175,6 +217,98 @@ def calc_cp_qe_rot_vel(quad_geo_mesh, q):
             tmp_arr.append((1./ A_m[m]) * w[m] * np.dot(w[m], tmp_omega))
         omega_vec += -4. * np.pi * np.sum(tmp_arr, axis=0)
     return omega_vec
+
+
+def calc_lp_qe_trans_vel(lin_pot_mesh, quad_geo_mesh, q):
+    """
+    Calculate the translational velocity of a particle.
+    Linear potential, quadratic elements
+    Parameters:
+        lin_pot_mesh: linear potential mesh
+        quad_geo_mesh: geometric mesh
+        q: DL potential solution; (3 * num_nodes,) ndarray
+    Returns:
+        part_v: particle translational velocity; (3,) ndarray
+    """
+    part_v = np.zeros(3)
+    pot_faces = lin_pot_mesh.get_faces()
+    num_faces = pot_faces.shape[0]
+    S_D = quad_geo_mesh.get_surface_area()
+    for face_num in range(num_faces):
+        face_nodes = quad_geo_mesh.get_tri_nodes(face_num)
+        face_hs = quad_geo_mesh.get_hs(face_num)
+        def make_v_sol_quad(face_num):
+            def v_quad(xi, eta, _nodes):
+                node_0 = pot_faces[face_num, 0]
+                node_1 = pot_faces[face_num, 1]
+                node_2 = pot_faces[face_num, 2]
+                phi_0 = geo.shape_func_linear(xi, eta, 0)
+                phi_1 = geo.shape_func_linear(xi, eta, 1)
+                phi_2 = geo.shape_func_linear(xi, eta, 2)
+                ret = (
+                    q[3 * node_0 : 3 * node_0 + 3] * phi_0 +
+                    q[3 * node_1 : 3 * node_1 + 3] * phi_1 +
+                    q[3 * node_2 : 3 * node_2 + 3] * phi_2
+                )
+                return ret
+            return v_quad
+        part_v += gq.int_over_tri_quad(
+            make_v_sol_quad(face_num),
+            face_nodes,
+            face_hs,
+        )
+    part_v *= (-4. * np.pi) / S_D
+    return part_v
+
+
+def calc_lp_qe_rot_vel(lin_pot_mesh, quad_geo_mesh, q):
+    """
+    Calculate the rotational velocity of a particle.
+    Linear potential, quadratic elements
+    Parameters:
+        lin_pot_mesh: linear potential mesh
+        quad_geo_mesh: quadratic geometric mesh
+        q: DL potential solutions; (3 * num_nodes,) ndarray
+    Returns:
+        omega_vec: particle rotational velocity; (3,) ndarray
+    """
+    num_faces = lin_pot_mesh.get_faces().shape[0]
+    pot_faces = lin_pot_mesh.get_faces()
+    x_c = quad_geo_mesh.get_centroid()
+    w = quad_geo_mesh.get_w()
+    A_m = quad_geo_mesh.get_A_m()
+    omega_vec = 0.
+    for face_num in range(num_faces):
+        face_nodes = quad_geo_mesh.get_tri_nodes(face_num)
+        face_hs = quad_geo_mesh.get_hs(face_num)
+        def make_omega_quad(face_num):
+            def omega_quad(xi, eta, nodes):
+                pos = geo.pos_linear(xi, eta, nodes)
+                X = pos - x_c
+                node_0 = pot_faces[face_num, 0]
+                node_1 = pot_faces[face_num, 1]
+                node_2 = pot_faces[face_num, 2]
+                phi_0 = geo.shape_func_linear(xi, eta, 0)
+                phi_1 = geo.shape_func_linear(xi, eta, 1)
+                phi_2 = geo.shape_func_linear(xi, eta, 2)
+                ret = (
+                    np.cross(X, phi_0 * q[3 * node_0 : 3 * node_0 + 3]) +
+                    np.cross(X, phi_1 * q[3 * node_1 : 3 * node_1 + 3]) +
+                    np.cross(X, phi_2 * q[3 * node_2 : 3 * node_2 + 3])
+                )
+                return ret
+            return omega_quad
+        tmp_omega = gq.int_over_tri_quad(
+            make_omega_quad(face_num),
+            face_nodes,
+            face_hs,
+        )
+        tmp_arr = []
+        for m in range(3):
+            tmp_arr.append((1./ A_m[m]) * w[m] * np.dot(w[m], tmp_omega))
+        omega_vec += -4. * np.pi * np.sum(tmp_arr, axis=0)
+    return omega_vec
+
 
 
 def calc_le_Am_vec(lin_geo_mesh):

rigid_DL/quad_geo_mesh.py
@@ -24,10 +24,13 @@ class Quad_Geo_Mesh(Geo_Mesh):
         """
         self.verts = np.array(x) # (Nv, 3) ndarray
         self.faces = np.array(f) # (Nf, 6) ndarray
-        self.centroid = self.calc_mesh_centroid_pc()
         self.quad_n = self.calc_all_quad_n()
         self.quad_hs = self.calc_all_quad_hs()
         self.surf_area = self.calc_surf_area()
+        self.centroid = self.calc_mesh_centroid()
+        print("Center of mass:")
+        print(self.centroid)
+        self.flip_n()
         self.mom_inertia = self.calc_moment_inertia_tensor()
         (self.w, self.A_m) = self.calc_rotation_eig()
         print("Surface area:")
@@ -128,8 +131,7 @@ class Quad_Geo_Mesh(Geo_Mesh):
         normals = np.empty([Nf, 3, 6])
         for i, face in enumerate(self.faces):
             nodes = self.get_tri_nodes(i)
-            tri_c = self.get_tri_center(i)
-            normals[i] = gq.quad_n(nodes, self.centroid, tri_c)
+            normals[i] = gq.quad_n(nodes)
         return normals
 
 
@@ -140,6 +142,24 @@ class Quad_Geo_Mesh(Geo_Mesh):
         """
         assert self.quad_n.any()
         return np.linalg.norm(self.quad_n, axis=1)
+
+
+    def flip_n(self):
+        """
+        Checks the orientation of the normals and reorients them to point
+        outwards from the mesh if required.
+        """
+        count = 0
+        for i, face in enumerate(self.faces):
+            x_c2tri = self.get_tri_center(i) - self.centroid
+            normals = self.quad_n[i]
+            for j in range(normals.shape[1]): #iterate over 6 normal vectors
+                n = normals[:, j]
+                if np.dot(n, x_c2tri) < 0.:
+                    count += 1
+                    self.quad_n[i][:,j] *= -1.
+        if count > 0:
+            print("flipped {} normal vectors".format(count))
 
 
     def calc_surf_area(self):